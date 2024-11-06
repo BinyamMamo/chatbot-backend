dbd60275dccfb3c5d80ccdcb15defc3157f37166
BinyamMamo <binyammamo01@gmail.com>
2024-11-06 23:44:51 +0400

    add google genai to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pip install Flask torch torchvision nltk python-dotenv
+pip install -q -U google-generativeai
 python -c "import nltk; nltk.download('punkt')"
 python -c "import nltk; nltk.download('punkt_tab')"  # for windows
 python chatbot/train.py
