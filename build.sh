ab421c2cd25d401b3cfac7d50af6e520dad1281c
BinyamMamo <binyammamo01@gmail.com>
2024-11-06 23:17:03 +0400

    fixes to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pip install Flask torch torchvision nltk
 python -c "import nltk; nltk.download('punkt')"
+python -c "import nltk; nltk.download('punkt_tab')"  # for windows
 python chatbot/train.py
