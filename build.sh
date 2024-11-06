176fa41d80d2c00c7cd5000755e4fdcaf788e67f
BinyamMamo <binyammamo01@gmail.com>
2024-11-06 23:31:49 +0400

    add dotenv to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip install Flask torch torchvision nltk
+pip install Flask torch torchvision nltk python-dotenv
 python -c "import nltk; nltk.download('punkt')"
 python -c "import nltk; nltk.download('punkt_tab')"  # for windows
 python chatbot/train.py
